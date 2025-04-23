daa34276ade99e14c673bcf16d5cdc93f310982c
Fabiofiorentino <fabiovilaas@gmail.com>
2025-04-23 10:16:41 -0300

    configurando banco de dados

---
+++ app/Models/Fornecedor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fornecedor extends Model
+{
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\ProdutoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('fornecedores', FornecedorController::class);
+Route::resource('produtos', ProdutoController::class);
